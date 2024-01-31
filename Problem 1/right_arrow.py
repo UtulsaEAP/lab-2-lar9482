1c8f5107919cc9a43ec9c7a68cd08a5e8f77578f
lar9482 <lar9482@utulsa.edu>
2024-01-31 14:22:59 -0600

    Passing test cases for problem 1

--- Problem 1/right_arrow.py
+++ Problem 1/right_arrow.py
@@ -1,10 +1,18 @@
 def right_arrow():
-    base_char = input()
-    head_char = input()
+    base_char = str(input())
+    head_char = str(input())
 
     row1 = '      ' + head_char
     ''' Type your code here. '''
-
+    row2 = ''
+    row3 = ''
+    for _ in range(0, 6): 
+        row2 += base_char
+        row3 += base_char
+    
+    row2 = row2 + head_char + head_char
+    row3 = row3 + head_char + head_char + head_char
+    
     print(row1)
     print(row2)
     print(row3)
