0c16294ed20dbbccc12e25a2459c384b25ef38a5
lar9482 <lar9482@utulsa.edu>
2024-01-31 14:27:29 -0600

    Passing test cases for problem 2

--- Problem 2/telephone.py
+++ Problem 2/telephone.py
@@ -1,5 +1,12 @@
 def telephone():
     phone_number = int(input())
     ''' Type your code here. '''
+
+    thirdNumber = phone_number % 10000
+    secondNumber = (phone_number // 10000) % 1000
+    firstNumber = (phone_number // 10000000)
+
+    print('({0}) {1}-{2}'.format(firstNumber, secondNumber, thirdNumber))
+
 if __name__ == "__main__":
     telephone()
